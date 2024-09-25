0935d63e0b883b473a49f96e36a55d658b2403a5
rbutle16 <rbutle16@emich.edu>
2024-09-25 14:39:17 -0400

    Programmer 1 messing with programmer 2

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld
 	{
 		System.out.println("Howdy GitHub project!");
 		System.out.println("Line 2");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this, Programmer 1 did this");
 	}
 
 }
