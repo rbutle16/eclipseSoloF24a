6659bbc68d9a35f2a2c653f1191c9e0910cec2a9
rbutle16 <rbutle16@emich.edu>
2024-09-25 14:44:58 -0400

    Error maybe thing?

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld
 	{
 		System.out.println("Howdy GitHub project!");
 		System.out.println("Line 2");
-		System.out.println("Programmer 2 did this, ProgrammerOne did this");
+		System.out.println("Programmer 2 did this ProgrammerOne did this");
 	}
 
 }
